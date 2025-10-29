347ccc11271b0877d363754a34d64ac4ff57f91e
eden <o.migisha@alustudent.com>
2025-10-29 09:00:56 -0700

    Task 0: safely print x elements of a list using try/except

--- python-more_data_structures/python-exceptions/0-safe_print_list.py
+++ python-more_data_structures/python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num_returned = 0
+    count = 0
     for i in range(x):
         try:
-            print(f"{my_list[i]}", end="")
-            num_returned += 1
-        except IndexError as IE:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
             break
     print()
-    return num_returned
+    return count
