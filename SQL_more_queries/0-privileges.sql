cae785cb1d93858a2385b8f14513f738563bd00b
eden <o.migisha@alustudent.com>
2025-11-28 22:51:43 -0800

    Add script to show privileges for user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,10 @@
+-- 0-privileges.sql
 -- Script to list privileges of user_0d_1 and user_0d_2 on localhost
 
--- For user_0d_1
+-- Label for user_0d_1
+SELECT 'Grants for user_0d_1@localhost:' AS info;
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- For user_0d_2
+-- Label for user_0d_2
+SELECT 'Grants for user_0d_2@localhost:' AS info;
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
