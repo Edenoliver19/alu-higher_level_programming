eef94028b5d1195e827cf9c327142ef499546e82
eden <o.migisha@alustudent.com>
2025-11-30 07:36:31 -0800

    Task 7: list all State objects using SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all State objects from the database hbtn_0e_6_usa"""
+"""List all State objects from the database"""
 
 import sys
 from sqlalchemy import create_engine
@@ -11,12 +11,13 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost/{database}")
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database)
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
     states = session.query(State).order_by(State.id).all()
-
     for state in states:
         print(f"{state.id}: {state.name}")
 
