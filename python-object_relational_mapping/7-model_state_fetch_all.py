c0c27ae8a8d5e04558aa840a731229c9e3d2d0de
eden <o.migisha@alustudent.com>
2025-11-30 07:34:20 -0800

    Task 7: list all State objects using SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,43 +1,23 @@
 #!/usr/bin/python3
-"""lists all State objects from the database
-hbtn_0e_6_usa"""
+"""Lists all State objects from the database hbtn_0e_6_usa"""
+
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    session = Session(engine)
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost/{database}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    records = session.query(State).all()
+    states = session.query(State).order_by(State.id).all()
 
-    for data in records:
-        print("{}: {}".format(data.__dict__['id'], data.__dict__['name']))
-
-    session.close()1~#!/usr/bin/python3
-"""lists all State objects from the database
-hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
-
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-
-    records = session.query(State).all()
-
-    for data in records:
-        print("{}: {}".format(data.__dict__['id'], data.__dict__['name']))
+    for state in states:
+        print(f"{state.id}: {state.name}")
 
     session.close()
