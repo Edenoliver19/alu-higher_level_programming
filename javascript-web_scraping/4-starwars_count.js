3d1d044745d68ca83581b2b4142312a16e9b10c8
eden <o.migisha@alustudent.com>
2026-02-06 05:38:42 -0800

    Add script to count Wedge Antilles appearances

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,47 +4,21 @@ const request = require('request');
 
 const url = process.argv[2];
 const characterId = '18';
-let count = 0;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
+request.get(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.log(err);
     return;
   }
 
-  const data = JSON.parse(body);
-  const films = data.results;
+  let count = 0;
 
-  films.forEach(film => {
-    const characters = film.characters;
-    if (characters.some(char => char.includes(`/people/${characterId}/`))) {
-      count++;
-    }
-  });
-
-  console.log(count);
-});01~#!/usr/bin/node
-
-const request = require('request');
-
-const url = process.argv[2];
-const characterId = '18';
-let count = 0;
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const films = data.results;
-
-  films.forEach(film => {
-    const characters = film.characters;
-    if (characters.some(char => char.includes(`/people/${characterId}/`))) {
-      count++;
-    }
+  body.results.forEach(movie => {
+    movie.characters.forEach(character => {
+      if (character.includes(`/people/${characterId}/`)) {
+        count++;
+      }
+    });
   });
 
   console.log(count);
